6c73b346f97e747077d94e12c27069cf179a8c80
Robbie Gollan <robbiegollan@gmail.com>
2023-12-31 15:55:30 +1100

    adds heading styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import './App.css';
-import Button from './components/ui/Button';
+import Styles from './Styles';
 
 function App() {
   return (
-    <div className="flex flex-col items-center">
-      <h1 className="text-4xl">Hello World</h1>
-      <div className="mt-2 flex gap-1">
-        <Button variant="primary">Primary</Button>
-        <Button variant="secondary">Secondary</Button>
-        <Button variant="danger">Danger</Button>
-        <Button variant="caution">Caution</Button>
-        <Button variant="gradient">Gradient</Button>
-      </div>
-    </div>
+    <>
+      <Styles />
+    </>
   );
 }
 
