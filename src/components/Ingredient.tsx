5c83b35e398252b37a3a70e006f96c1baeba0575
Robbie Gollan <robbiegollan@gmail.com>
2024-01-01 20:07:53 +1100

    adds additional styling to Ingredient component

--- src/components/Ingredient.tsx
+++ src/components/Ingredient.tsx
@@ -13,20 +13,27 @@ export interface IngredientData {
 
 const Ingredient = ({ data }: Props) => {
   const { name, effect1, effect2, effect3, effect4, src } = data;
+
   return (
-    <div className="flex w-full gap-2 border border-neutral-800 p-2">
-      <img src={src} alt="Ingredient icon" className="h-12 w-12" />
-      <div className="flex w-1/3 items-center">
-        <p className="font-bold">{name}</p>
-      </div>
-      <div className="flex w-full flex-col p-1">
-        <div className="flex">
-          <p className="w-1/2 text-sm text-neutral-500">{effect1}</p>
-          <p className="w-1/2 text-sm text-neutral-500">{effect2}</p>
+    <div className="flex w-full border border-neutral-800 px-2 shadow-sm hover:cursor-pointer hover:bg-green-100 hover:shadow-md">
+      <div className="flex w-2/3 justify-start gap-2">
+        <div className="h-max">
+          <img src={src} alt="Ingredient icon" className="h-full w-auto" />
         </div>
-        <div className="flex">
-          <p className="w-1/2 text-sm text-neutral-500">{effect3}</p>
-          <p className="w-1/2 text-sm text-neutral-500">{effect4}</p>
+        <div className="flex items-center">
+          <p className="font-bold">{name}</p>
+        </div>
+      </div>
+      <div className="flex w-1/3 items-center">
+        <div className="flex flex-col">
+          <div className="flex flex-col">
+            <p className="text-xs text-neutral-500">{effect1}</p>
+            <p className="text-xs text-neutral-500">{effect2}</p>
+          </div>
+          <div className="flex flex-col">
+            <p className="text-xs text-neutral-500">{effect3}</p>
+            <p className="text-xs text-neutral-500">{effect4}</p>
+          </div>
         </div>
       </div>
     </div>
