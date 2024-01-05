cfb6fbf6ba201747ec921092245182c7fc060e5b
Robbie Gollan <robbiegollan@gmail.com>
2024-01-05 12:18:45 +1100

    adds styling and deselecting to selected ingredients

--- src/components/IngredientCard.tsx
+++ src/components/IngredientCard.tsx
@@ -1,14 +1,24 @@
+import { useContext } from 'react';
 import { IngredientData } from './IngredientTile';
+import { LogicContext } from '../context/LogicContext';
 interface Props {
   data: IngredientData;
 }
 
 const IngredientCard = ({ data }: Props) => {
   const { effect1, effect2, effect3, effect4, src } = data;
+  const { removeSelectedIngredient } = useContext(LogicContext);
+
+  const clickHandler = () => {
+    removeSelectedIngredient(data.id);
+  };
 
   return (
-    <div className="flex w-full items-center gap-2 border-t border-black py-1">
-      <div className="h-10 w-1/5">
+    <div
+      onClick={clickHandler}
+      className="m-1 flex w-full cursor-pointer items-center gap-2 border-2 border-gray-200 py-1 shadow-sm hover:border-red-200 hover:shadow-md"
+    >
+      <div className="mr-1 h-10">
         <img src={src} alt="Ingredient icon" className="w-10" />
       </div>
       <div className="flex w-2/5 flex-col text-xs">
--- src/components/SelectedList.tsx
+++ src/components/SelectedList.tsx
@@ -7,13 +7,13 @@ const SelectedList = () => {
   const { selectedIngredients } = useContext(LogicContext);
 
   return (
-    <div className="flex h-48 flex-col gap-2 border border-black py-1">
+    <div className="flex h-56 flex-col border border-black p-1 pr-3">
       <Heading variant="h3" className="text-center">
         Selected Ingredients
       </Heading>
       <div className="flex flex-col">
-        {selectedIngredients.map((ing) => {
-          return <IngredientCard data={ing} />;
+        {selectedIngredients.map((data) => {
+          return <IngredientCard key={`selected: ${data.id}`} data={data} />;
         })}
       </div>
     </div>
