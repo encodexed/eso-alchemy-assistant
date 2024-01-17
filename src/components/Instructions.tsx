50e92d314a35044ddff9d4e7acc12505b18c4f04
Robbie Gollan <robbiegollan@gmail.com>
2024-01-17 15:00:50 +1100

    minor style adjustment

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -3,7 +3,7 @@ const Instructions = () => {
     <div className="border border-gray-300 p-1">
       <p className="text-sm">
         <span className="font-bold text-green-500">Green</span> indicates
-        compatibility.
+        compatibility effects between ingredients.
         <br />
         <span className="font-bold text-red-500">Red</span> indicates this
         effect is nullifying another effect.
